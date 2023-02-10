c9ead0cc9ba34b78e74299eceb5ba93e4f9e892c
Christoph Godecke <cgodecke@c2s16d93.42wolfsburg.de>
2023-02-10 15:08:35 +0100

    rx_rrx_costs

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 18:03:32 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/02/09 17:45:40 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/02/10 15:06:11 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,51 @@ void	pb(t_psw_list **a_list, t_psw_list **b_list)
 	*a_list = tmp;
 }
 
-void update_position(t_psw_list *a_list, t_psw_list *b_list)
+
+t_psw_list	*find_node_b(int content_a, t_psw_list *b_list)
+{
+	int			max_content;
+	int 		max_max_content;
+	t_psw_list	*max_node;
+	t_psw_list	*max_max_node;
+
+	max_content = 0;
+	max_max_content = 0;
+	while (b_list != NULL)
+	{
+		if (b_list->content > max_max_content)
+		{
+			max_max_content = b_list->content;
+			max_max_node = b_list;
+		}
+		if (b_list->content < content_a && b_list->content > max_content)
+		{
+			max_content = b_list->content;
+			max_node = b_list;
+		}
+		b_list = b_list->next;
+	}
+	if (max_content == 0)
+		return (max_max_node);
+	return (max_node);
+}
+
+void	clear_values(t_psw_list *lst)
+{
+	while (lst != NULL)
+	{
+		lst->costs = 0;
+		lst->ra = 0;
+		lst->rra = 0;
+		lst->rb = 0;
+		lst->rrb = 0;
+		lst->rr = 0;
+		lst->rrr = 0;
+		lst->node_b = NULL;
+		lst = lst->next;
+	}
+}
+void	update_lists(t_psw_list *a_list, t_psw_list *b_list)
 {
 	int		i;
 
@@ -45,6 +89,7 @@ void update_position(t_psw_list *a_list, t_psw_list *b_list)
 	while (a_list != NULL)
 	{
 		a_list->index = i;
+		a_list->node_b = find_node_b(a_list->content, b_list);
 		a_list = a_list->next;
 		i++;
 	}
@@ -55,12 +100,68 @@ void update_position(t_psw_list *a_list, t_psw_list *b_list)
 		b_list = b_list->next;
 		i++;
 	}
+	clear_values(a_list);
+}
+
+
+
+	void	rx_rrx_costs(t_psw_list *a_list, t_psw_list *b_list)
+	{
+		t_psw_list	*a_list_start;
+
+		a_list_start = a_list;
+		while(a_list != NULL)
+		{
+			if (a_list->index - 1 <= (psw_lstsize(a_list_start) - a_list->index + 1))   // Am ende prüfen, ob die +1 weggelassen werden kann. müsste eigentlich.
+			{
+				//printf("hallo");
+				a_list->ra = a_list->index - 1;
+			}
+			else
+			{
+				//printf("olla:%i", psw_lstsize(a_list_start));
+				a_list->rra = psw_lstsize(a_list_start) - a_list->index + 1;
+			}
+			if (a_list->node_b->index - 1 < (psw_lstsize(b_list) - a_list->node_b->index + 1))
+				a_list->rb = a_list->node_b->index - 1;
+			else
+				a_list->rrb = psw_lstsize(b_list) - a_list->node_b->index + 1;
+			a_list->costs = a_list->ra + a_list->rra + a_list->rb + a_list->rrb + a_list->rr + a_list->rrr;
+			a_list = a_list->next;
+		}
+	}
+
+t_psw_list	*cheapest_node(t_psw_list *a_list, t_psw_list *b_list)
+{
+	t_psw_list	*cheapest;
+	int			min_costs;
+
+	
+	//#A: berechnen ob ra oder rra kurzer ist.
+	//#B: berechnen ob rb oder rrb kurzer ist.
+	//# Kosten A & B addieren.
+	rx_rrx_costs(a_list, b_list);
+
+	min_costs = a_list->costs;
+	cheapest = a_list;
+	while (a_list != NULL)
+	{
+		if (a_list->costs < min_costs)
+		{
+			min_costs = a_list->costs;
+			cheapest = a_list;
+		}
+		a_list = a_list->next;
+	}
+	
+	return (cheapest);
 }
 
 int main(int argc, char **argv)
 {
-	t_psw_list *a_list;
-	t_psw_list *b_list;
+	t_psw_list	*a_list;
+	t_psw_list	*b_list;
+	t_psw_list	*node_a_to_push;
 
 //#linked list erstellen
 	a_list = create_list(argv + 1);
@@ -68,32 +169,28 @@ int main(int argc, char **argv)
 	pb(&a_list, &b_list);
 	pb(&a_list, &b_list);
 //#positionsinfo beider stacks aktuallisieren
-	update_position(a_list, b_list);
-
-
+	update_lists(a_list, b_list);
+//Kosten berechnen
+	node_a_to_push = cheapest_node(a_list, b_list);
 
 
 //Print stacks:
 	printf("STACK A:\n");
-	printf("%i\n", a_list->content);
-	printf("index:%i\n", a_list->index);
-		while(a_list->next != NULL)
+	printf("%i index:%i costs:%i ra:%i rra:%i rb:%i rrb:%i rr:%i rrr:%i content_b:%i\n", a_list->content, a_list->index, a_list->costs, a_list->ra, a_list->rra, a_list->rb, a_list->rrb, a_list->rr, a_list->rrr, a_list->node_b->content);
+	while(a_list->next != NULL)
 	{
 		a_list = a_list->next;
 		
-		printf("%i\n", a_list->content);
-		printf("index:%i\n", a_list->index);
+		printf("%i index:%i costs:%i ra:%i rra:%i rb:%i rrb:%i rr:%i rrr:%i content_b:%i\n", a_list->content, a_list->index, a_list->costs, a_list->ra, a_list->rra, a_list->rb, a_list->rrb, a_list->rr, a_list->rrr, a_list->node_b->content);
 	}
 
 	printf("STACK B:\n");
-	printf("%i\n", b_list->content);
-	printf("index:%i\n", b_list->index);
+	printf("%i index:%i costs:%i ra:%i rra:%i rb:%i rrb:%i rr:%i rrr:%i\n", b_list->content, b_list->index, b_list->costs, b_list->ra, b_list->rra, b_list->rb, b_list->rrb, b_list->rr, b_list->rrr);
 	while(b_list->next != NULL)
 	{
 		b_list = b_list->next;
 		
-		printf("%i\n", b_list->content);
-		printf("index:%i\n", b_list->index);
+		printf("%i index:%i costs:%i ra:%i rra:%i rb:%i rrb:%i rr:%i rrr:%i\n", b_list->content, b_list->index, b_list->costs, b_list->ra, b_list->rra, b_list->rb, b_list->rrb, b_list->rr, b_list->rrr);
 	}
 	
 
@@ -103,11 +200,15 @@ int main(int argc, char **argv)
 		#zwei nach B schieben
 
 		#positionsinfo beider stacks aktuallisieren
+		# Note in B finden über den A hinzugefügt werden muss
 	
 	Kosten berechnen
 	#A: berechnen ob ra oder rra kurzer ist.
 	#B: berechnen ob rb oder rrb kurzer ist.
-	#berechnen, welche kombination am wenigsten kostet (ra, ra)
+	# Kosten A & B addieren.
+	#berechnen, ob durch rr oder rrr kosten reduziert werden können:
+		#größerer index sind die max kosten. (rr+rX)
+		#größerer (list len - index) sind die max kosten (rrr+rrX).
 
 	# die günstigste Zahl aus A nehmen und eneut günstigste kombination berechnen 
 	#Rotation und swap durchführen
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 16:45:02 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/02/09 17:31:59 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/02/10 14:44:23 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,15 @@ typedef struct s_psw_list
 	int					content;
 	int					index;
 
+	int 				costs;
+	int					ra;
+	int					rra;
+	int					rb;
+	int					rrb;
+	int					rr;
+	int					rrr;
+	struct s_psw_list	*node_b;
+
 	struct s_psw_list	*next;
 }						t_psw_list;
 
@@ -32,6 +41,7 @@ typedef struct s_psw_list
 t_psw_list	*psw_lstnew(int content);
 t_psw_list	*psw_lstlast(t_psw_list *lst);
 t_psw_list	*create_list(char **argv);
+int	psw_lstsize(t_psw_list *lst);
 void	psw_lstadd_back(t_psw_list **lst, t_psw_list *new);
 void	psw_lstadd_front(t_psw_list **lst, t_psw_list **new);
 
